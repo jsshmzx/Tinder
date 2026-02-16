467710878c2d84f340370326cadb5b7315cf3eb7
Teror Fox <i@trfox.top>
2026-02-16 14:53:22 +0800

    重命名 'time' 列为 'vote_time'，重命名 'status' 列为 'current_status'，重命名 'type' 列为 'looking_for_type'

--- core/db_migration/SQL/initial_vote.sql
+++ core/db_migration/SQL/initial_vote.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS vote (
     id SERIAL PRIMARY KEY,
     uuid TEXT NOT NULL UNIQUE,
     vote_type TEXT,
-    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    vote_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     committed_by TEXT,
     content TEXT
 );
--- core/db_migration/SQL/initial_wall_looking_for.sql
+++ core/db_migration/SQL/initial_wall_looking_for.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS wall_looking_for (
     id SERIAL PRIMARY KEY,
     uuid TEXT NOT NULL UNIQUE,
-    status TEXT,
+    current_status TEXT,
     real_status TEXT,
     seeker TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    type TEXT,
+    looking_for_type TEXT,
     last_seen_time TIMESTAMP,
     helper TEXT,
     clues TEXT
